07fa2b9f3151985b4767effdbb83581edcef60a8
Martin Westgate <martinjwestgate@gmail.com>
2019-11-19 11:41:56 +1100

    add scripts for importing data drills from remote sensing

---
+++ functions/csv_import.R
@@ -0,0 +1,61 @@
+# set some functions to cleanly import remote sensing (rs) data from .csv
+
+library(lubridate) # required
+
+# get farm locations including site name, lat and long
+# this currently isn't used because we have this info in other datasets;
+# but is retained here in case it comes in handy later
+get_locations <- function(
+  x # must be a csv
+){
+  data_raw <- read.table(
+    x,
+    stringsAsFactors = FALSE,
+    skip = 5,
+    nrows = 2,
+    sep = ",",
+    header = TRUE
+  )[, -c(1:4)]
+
+  data_clean <- data.frame(
+    site = colnames(data_raw),
+    latitude = as.numeric(data_raw[1, ]),
+    longitude = as.numeric(data_raw[2, ]),
+    stringsAsFactors = FALSE
+  )
+
+  return(data_clean)
+}
+
+
+# import remote sensing data from those locations
+get_observations <- function(x){
+  data_raw <- read.table(
+    x,
+    stringsAsFactors = FALSE,
+    skip = 5,
+    sep = ",",
+    header = TRUE
+  )[-c(1:3), -4]
+  colnames(data_raw)[1:3] <- c("year", "month", "day")
+
+  # sort out dates
+  data_raw <- data_raw[
+      !apply(data_raw[, 1:3], 1, function(a){all(as.character(a) == "")}),
+    ]
+  data_raw$date <- lubridate::ymd(
+    apply(data_raw[, 1:3], 1,
+      function(a){paste(as.character(a), collapse = "-")}
+    )
+  )
+  data_raw <- data_raw[, c(ncol(data_raw), 1:(ncol(data_raw)-1))]
+
+  data_long <- data.frame(
+    site = rep(colnames(data_raw)[-c(1:4)], each = nrow(data_raw)),
+    date = rep(data_raw$date, ncol(data_raw)-4),
+    value = do.call(c, data_raw[, -c(1:4)]),
+    stringsAsFactors = FALSE
+  )
+
+  return(data_long)
+}
---
+++ scripts/1_data_import_and_matching.R
@@ -0,0 +1,63 @@
+# script to import and clean remote sensing data
+# Note: this requires functions stored in "./functions/csv_import.R"
+
+# Gross Primary Productivity
+gpp_values <- get_observations("./data_raw/Birdsite_GPP_FMC_pointdrills_GPP.csv")
+gpp_values$site <- sub("\\.", "-", gpp_values$site)
+colnames(gpp_values)[3] <- "gpp"
+
+# Fuel Moisture Content
+fmc_values <- get_observations("./data_raw/Birdsite_GPP_FMC_pointdrills_FMC.csv")
+fmc_values$site <- sub("\\.", "-", fmc_values$site)
+colnames(fmc_values)[3] <- "fmc"
+
+# any(is.na(fmc_values$site))
+# any(is.na(gpp_values$site)) # checks
+
+# import site data
+sites <- readRDS("./data/sws_sites.rds")
+
+
+# # try merging by site and date
+# test_sites <- merge(sites, gpp_values,
+#   by.x = c("SiteCode", "SurveyDate"),
+#   by.y = c("site", "date"),
+#   all.x = TRUE,
+#   all.y = FALSE
+# )
+# fails, apparently because the dates in rs data do not exactly match those in the monitoring data
+# ergo we need to match to the closest possible date
+
+# set up locations in site data to add new variables
+sites$date <- lubridate::ymd(sites$SurveyDate)
+sites$gpp <- NA
+sites$fmc <- NA
+site_list <- split(sites, seq_len(nrow(sites)))
+
+# use lapply to find the nearest observation of each variable
+# to an observation in our dataset
+# a <- site_list[[2]] # testing
+site_list2 <- lapply(site_list, function(a, gpp, fmc){
+  # gpp
+  site_test <- gpp$site == a$SiteCode
+  if(any(site_test)){
+    rows1 <- which(site_test)
+    row <- rows1[which.min(as.numeric(gpp$date[rows1] - a$date)^2)]
+    a$gpp <- gpp$gpp[row]
+  }
+  # fmc
+  site_test <- gpp$site == a$SiteCode
+  if(any(site_test)){
+    rows1 <- which(site_test)
+    row <- rows1[which.min(as.numeric(fmc$date[rows1] - a$date)^2)]
+    a$fmc <- fmc$fmc[row]
+  }
+  return(a)
+},
+fmc = fmc_values,
+gpp = gpp_values
+)
+
+sites_rs <- do.call(rbind, site_list2)
+# sites_rs$year <- as.numeric(sites_rs$date)
+# length(which(is.na(sites_rs$gpp)))
