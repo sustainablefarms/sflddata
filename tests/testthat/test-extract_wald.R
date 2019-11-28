5a8001d8cdafd190d8788ee478654e8795633533
Kassel Hingee <hingeek@fses.anu.edu.au>
2019-11-29 10:02:06 +1100

    added the (failing) tests comparing to data extracted by Marta

--- tests/testthat/test-extract_wald.R
+++ tests/testthat/test-extract_wald.R
@@ -30,3 +30,62 @@ test_that("Annual minimum temperature ARCH-1 is extracted correctly (this file's
 })
 
 ## Would love another test here of GPP data, but ncdf4 can't open GPP file: http://dapds00.nci.org.au/thredds/dodsC/ub8/au/OzWALD/8day/GPP/OzWALD.GPP.2000.nc
+
+test_that("extract_ts_wald() matches GPP values extracted by Marta (dimensions transposed)", {
+  # Marta-Extracted FMC data 
+  df <- read.table("./data/Birdsite_GPP_FMC_pointdrills_GPP.csv",
+                   skip = 5, 
+                   blank.lines.skip = TRUE, 
+                   sep = c(",", "\n"),
+                   header = TRUE)
+  colnames(df)[1:3] <- c("year", "month", "day")
+  colnames(df) <- sub("\\.", "-", colnames(df))
+  longlats <- df[1:2, 5:ncol(df)]
+  row.names(longlats) <- df[1:2, 4]
+  gppvals <- df[4:nrow(df),-4]
+  
+  # Kass-Extracted FMC Data
+  source("./functions/sites2spatialpoints.R")
+  source("./functions/extract_ts_wald.R")
+  sws_sites <- readRDS("./data/sws_sites.rds")
+  points <- swssites2spdf(sws_sites)[1:10, ]
+  tseries <- extract_ts_wald(points,
+                             "http://dapds00.nci.org.au/thredds/dodsC/ub8/au/OzWALD/8day/GPP/OzWALD.GPP.2000.nc",
+                             varname = "GPP",
+                             nl = 2) 
+  
+  expect_equivalent(t(tseries)[, "ARCH-1"], gppvals[1:2, "ARCH-1"])
+  expect_equivalent(t(tseries)[, "ARCH-2"], gppvals[1:2, "ARCH-2"])
+  expect_equivalent(t(tseries)[, "ARCH-3"], gppvals[1:2, "ARCH-3"])
+})
+
+test_that("extract_ts_wald() matches FMC values extracted by Marta (dimensions not transposed)", {
+  # Marta-Extracted FMC data 
+  df <- read.table("./data/Birdsite_GPP_FMC_pointdrills_FMC.csv",
+                   skip = 5, 
+                   blank.lines.skip = TRUE, 
+                   sep = c(",", "\n"),
+                   header = TRUE)
+  colnames(df)[1:3] <- c("year", "month", "day")
+  colnames(df) <- sub("\\.", "-", colnames(df))
+  longlats <- df[1:2, 5:ncol(df)]
+  row.names(longlats) <- df[1:2, 4]
+  fmcvals <- df[4:nrow(df),-4]
+  
+  # Kass-Extracted FMC Data
+  source("./functions/sites2spatialpoints.R")
+  source("./functions/extract_ts_wald.R")
+  sws_sites <- readRDS("./data/sws_sites.rds")
+  points <- swssites2spdf(sws_sites)[1:2, ]
+  tseries <- extract_ts_wald(points,
+                             "http://dapds00.nci.org.au/thredds/dodsC/ub8/au/FMC/c6/mosaics/fmc_c6_2001.nc",
+                             varname = "fmc_mean",
+                             nl = 2) 
+  tseriesdates <- as.POSIXlt(as.numeric(gsub("X", "", colnames(tseries))), origin = lubridate::origin)
+  colnames(tseries) <- as.character(tseriesdates)
+  
+  expect_equivalent(t(tseries)[, "ARCH-1"], fmcvals[1:2, "ARCH-1"])
+  expect_equivalent(t(tseries)[, "ARCH-2"], fmcvals[1:2, "ARCH-2"])
+  #t(tseries)
+  #fmcvals[1:2, row.names(tseries)]
+})
