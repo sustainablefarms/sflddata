8a85a11505eea3d65f5cf2665839639873d6f563
Kassel Hingee <kassel.hingee@gmail.com>
2019-11-29 16:14:02 +1100

    sites_2_sp_points removes the dash in site code and row name

--- functions/sites_2_sp_points.R
+++ functions/sites_2_sp_points.R
@@ -15,6 +15,7 @@ sws_sites_2_spdf <- function(swssite_dataframe){
   # remove all the sites that do not have a complete spatial location:
   sws_sites_cleaned <- swssite_dataframe[!(is.na(swssite_dataframe[,"longitude"]) | is.na(swssite_dataframe[,"latitude"])), ]
   sws_sites_cleaned <- sws_sites_cleaned[!(duplicated(sws_sites_cleaned$longitude) & duplicated(sws_sites_cleaned$latitude)), ]
+  sws_sites_cleaned$SiteCode <- gsub("-","", sws_sites_cleaned$SiteCode) # remove dashes because it stuffs up variable calling in R sometimes
   row.names(sws_sites_cleaned) <- sws_sites_cleaned$SiteCode
   sws_sites_spdf <- SpatialPointsDataFrame(sws_sites_cleaned[,c("longitude","latitude")], sws_sites_cleaned, proj4string = sp::CRS("+proj=longlat +datum=WGS84"))
   return(sws_sites_spdf)
