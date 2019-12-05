e515573967337b42a8bb9617389e8f07d073ba01
Kassel Hingee <hingeek@fses.anu.edu.au>
2019-12-06 09:38:40 +1100

    two functions for helping extract remote sensing data

---
+++ functions/build_filename_list.R
@@ -0,0 +1,31 @@
+#' @title Create list of Filenames
+#' @param server server url
+#' @param directory directory of data within server
+#' @param prefix start of filename
+#' @param middle middle of filename
+#' @param suffix end of filename
+#' @param type_extension The character string representing file type
+#' @param dirsep Character used to combine server and directory
+#' @param namesep Characer used to combine parts of the filenames
+#' @details server and directory are concatenated using dirsep. prefix, middle, suffix and type_extension are concatenated using namesep separator.
+#' Uses \code{paste}.
+#' @examples
+#' server <- "http://dapds00.nci.org.au/thredds/dodsC/ub8/au/OzWALD" #http://dapds00.nci.org.au/thredds/catalog/ub8/au/OzWALD/catalog.html
+#' directory <- "8day/GPP"
+#' prefix <- "OzWALD"
+#' middle <- "GPP"
+#' suffix <- 2000:2018
+#' type_extension <- "nc"
+#' build_filename_list("http://dapds00.nci.org.au/thredds/dodsC/ub8/au/OzWALD",
+#'       "8day/GPP",
+#'       "OzWALD",
+#'       "GPP",
+#'       2000:2018,
+#'       "nc")
+
+build_filename_list <- function(server, directory, prefix, middle, suffix, type_extension,
+                                dirsep = "/", namesep = "."){
+  dirname <- paste(server, directory, sep = dirsep)
+  filename <- paste(prefix, middle, suffix, type_extension, sep = namesep)
+  return(paste(dirname, filename, sep = dirsep))
+}
---
+++ functions/extract_brick_files.R
@@ -0,0 +1,33 @@
+#' @title Extract RasterBrick from List of Filenames
+#' @importFrom raster brick crop
+#' @param files A character list of filenames
+#' @param varname Character. Variable name to extract from each file.
+#' @param roi The region to extract. A \pkg{raster} 'extent' object.
+#' @param dims The order of dimensions that the variable is saved in. dims = c(1, 2, 3) is [longitude, latitude, time]. dims = c(2, 1, 3) is [latitude, longitude, time].
+#' @examples 
+#' roi <- extent(148.0779, 148.2011, -35.06581, -35.13167)
+#' files <- build_filename_list("http://dapds00.nci.org.au/thredds/dodsC/ub8/au/OzWALD",
+#'       "8day/GPP",
+#'       "OzWALD",
+#'       "GPP",
+#'       2000:2018,
+#'       "nc")
+#' b <- extract_brick_files(files, "GPP", roi, dims = c(2, 1, 3))
+
+
+extract_brick_files <- function(files, varname, roi, dims = 1:3){
+  #extract raster in region of interest and combine into a larger raster
+  roiras.l <- lapply(files, function(x) {
+    roiras <- tryCatch(
+      {b <- brick(x, varname = varname, dims = dims)
+      roiras <- crop(b, roi, snap = "out")
+      },
+      error = function(cond){
+        message(paste("Error in reading", x))
+        message(cond)
+        return(NULL)
+      })
+    return(roiras)})
+  
+  return(brick(roiras.l))
+}
