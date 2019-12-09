9d84ade2521049657f9dfbd02471e15a8b67948a
Kassel Hingee <kassel.hingee@gmail.com>
2019-12-09 16:07:57 +1100

    headway on Albers tiles extraction (Woody Cover)

--- scripts/1_7_extract_WoodyCover.R
+++ scripts/1_7_extract_WoodyCover.R
@@ -1,7 +1,7 @@
 # Extract Woody Cover
-# it is in Albers Tile format.
-# These are in folders xmin_ymin in Easting and Northings
-# according to https://spatialreference.org/ref/epsg/3577/: CRS("+init=epsg:3577") is GDA94 / Australia Albers. It matches the crs pulled from the netCDF files
+# it is in Albers Tile format with CRS = CRS("+init=epsg:3577").
+# (see analysis 1_2_checking_albers_tiles.Rmd for why)
+# Tiles are in folders xmin_ymin in Easting and Northings, where xmin and ymin are in '000 000s of meters.
 
 invisible(lapply(c("raster", "maptools", "rgdal", "ncdf4", "lubridate"),
        library, character.only = TRUE))
@@ -10,44 +10,58 @@ invisible(lapply(paste0("../linking-data/functions/", list.files("../linking-dat
 
 # Construct Region Desired
 sws_sites <- readRDS("../linking-data/data/sws_sites.rds")
-points <- spTransform(sws_sites_2_spdf(sws_sites), 
-                      CRS("+proj=aea +lat_1=-18 +lon_0=132 +lat_0=0 +x_0=0 +y_0=0 +a=6378137 +rf=298.257222101 +lat_2=45.5"))
+ptsraw <- sws_sites_2_spdf(sws_sites)
+points <- spTransform(sws_sites_2_spdf(sws_sites),
+                      CRS("+init=epsg:3577"))
 roi <- extent(points)
 
-# Bricks for each tile that overlays the sws points:
-files1 <- build_filename_list(
-  "http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover",
-  "DEA_ALC/13_-41",
-  "fc_metrics_13_-41_",
-  2000:2018,
-  ".nc",
-  namesep = "")
-source("./functions/extract_brick_files.R")
-b1 <- extract_brick_files(files1[1], varname = "WCF", roi, dims = c(2, 1), timeconvertfun = function(t) t)
+#tile codes:
+tilestep <- 100000
+lxmin <- floor(roi@xmin / tilestep) * tilestep #lowest xmin
+xmins <- seq(lxmin, -1 + ceiling(roi@xmax / tilestep) * tilestep,
+             by = tilestep)
+lymin <- floor(roi@ymin / tilestep) * tilestep #lowest ymin
+ymins <- seq(lymin, -1 + ceiling(roi@ymax / tilestep) * tilestep,
+             by = tilestep)
+
+xmin_v_ymin <- expand.grid(xmin = xmins, ymin = ymins)
+tilecodes <- apply(xmin_v_ymin / tilestep, 1, function(x) paste(x, collapse = "_"))
+names(tilecodes) <- tilecodes
 
 
 
-b1 <- raster("http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC/16_-39/fc_metrics_16_-39_2000.nc", varname = "WCF", dims = 2:1)
-b2 <- raster("http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC/17_-39/fc_metrics_17_-39_2000.nc", varname = "WCF", dims = 2:1)
-b3 <- raster("http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC/18_-39/fc_metrics_18_-39_2000.nc", varname = "WCF", dims = 2:1)
-b4 <- raster("http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC/14_-39/fc_metrics_14_-39_2000.nc", varname = "WCF", dims = 2:1)
-b.l <- list(b1, b2, b3, b4)
-m <- do.call(mosaic, c(b.l, fun = "mean"))
+#build brick for each tile
+brickfortile <- function(tilecode){
+  filelist <- build_filename_list("http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC",
+                      tilecode,
+                      paste0("fc_metrics_", tilecode, "_"),
+                      2000:2018,
+                      ".nc",
+                      namesep = "")
+  r.l <- lapply(filelist,
+    function(x){
+      tryCatch(
+        {ras <- raster(x, varname = "WCF", dims = 2:1)
+        return(ras)
+        }
+        ,
+        warning = function(w) {
+          if (!grep("cannot process these parts of the CRS", as.character(w))){
+            warning(paste("For", x, w))
+          } else {
+            suppressWarnings(ras <- raster(x, varname = "WCF", dims = 2:1))
+          }
+        })
+  })
+  bs <- brick(r.l)
+  bs <- crop(bs, roi)
+  names(bs) <- 2000:2018
+  return(bs)}
+b.l <- lapply(tilecodes, brickfortile) 
+save(b.l, file = "b.l.Rdata")
 
-# Check that points and extents overlap:
-nrow(intersect(points, extent(b1))) > 0
+# merge bricks
+b <- Reduce(merge, b.l)
+names(b) <- 2000:2018
 
 
-#
-
-crop(b1, roi, snap = "out")
-
-extract(m, points)
-
-plot(b2)
-plot(add = TRUE, points)
-plot(points)
-plot(add = TRUE, extent(b1))
-plot(add = TRUE, extent(b2))
-plot(add = TRUE, extent(b3))
-plot(add = TRUE, extent(b4))
