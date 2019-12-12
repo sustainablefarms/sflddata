3a477b7101522ef42412987129a74d8aa1c969d5
Kassel Hingee <hingeek@fses.anu.edu.au>
2019-12-12 12:59:19 +1100

    scrip for generating prediction figures appears to be working, but haven't checked for bugs.

--- scripts/3_1_apply_boral_fit_to_region.R
+++ scripts/3_1_apply_boral_fit_to_region.R
@@ -69,13 +69,12 @@ extent(gpp_diff) <- extent(fmc_mean)
 
 # woody cover
 spobj <- buffer(spTransform(points, CRS("+init=epsg:3577")), 1000)
-woodycover <- brick_woodycover(spobj, 2002)
+woodycover <- brick_woodycover(spobj, 2000:2018)
 ## compute average of buffer for every pixel
 wf <- focalWeight(woodycover, 500, type = "circle") 
 woodycover_smooth <- focal_bylayer(woodycover, wf, fun = sum)
-
-# convert woody cover smooth to the resolution of the other data sets
-
+names(woodycover_smooth) <- names(woodycover)
+woodycover_years <- year(as_date(names(woodycover), tz = "Australia/Canberra", format = "X%Y"))
 
 
 ##########################
@@ -86,36 +85,46 @@ names(gpp_diff) <- names(gpp)
 fmc_mean_diff <- calc(fmc_mean_diff, fun = prep_fmc_diff)
 names(fmc_mean_diff) <- names(fmc_mean)
 woodycover_smooth <- calc(woodycover_smooth, fun = prep_log_plus_one_woody_cover)
-# dates later
+names(woodycover_smooth) <- names(woodycover)
+# dates prepped later
 
 
 # spatial:
 #   go to coarse resolution: **this should be replicated into the preprocessing for the modelling data
 woodycover_smooth_lr <- projectRaster(woodycover_smooth, fmc_mean, method = "bilinear")
+names(woodycover_smooth_lr) <- names(woodycover_smooth)
 
 # time: work out dates available, cut down all data to those dates
 #  FMC and GPP are produced at the same time points,
 # then restrict to Woody Cover data assuming woody cover constant within a year
 fmc_gpp_times <- gpp_times[gpp_times %in% fmc_times]
-fmc_gpp_wc_times <- fmc_gpp_times[year(fmc_gpp_times) %in% 
-                 year(as_date(names(woodycover_smooth), tz = "Australia/Canberra", format = "X%Y"))]
+fmc_gpp_wc_times <- fmc_gpp_times[year(fmc_gpp_times) %in% woodycover_years]
+
+# cut down dates to bimonthly for computational reasons
+approx_desired_dates <- seq(fmc_gpp_wc_times[[1]],
+                            to = fmc_gpp_wc_times[[length(fmc_gpp_wc_times)]],
+                            by = "2 month")
+out_dates <- fmc_gpp_wc_times[vapply(approx_desired_dates, function(x) which.min(abs(fmc_gpp_wc_times - x)), FUN.VALUE = 3)]
 
 ### Now create/cut data based on the above dates
 # a stack of woody cover layers repeated for each time point
-woodycover_smooth_extra_dates_stack <- woodycover_smooth_lr[[paste0("X", year(fmc_gpp_wc_times)), ]]
+woodycover_smooth_extra_dates_stack <- woodycover_smooth_lr[[paste0("X", year(out_dates)), ]]
+names(woodycover_smooth_extra_dates_stack) <- out_dates
 
 # cut down fmc_diff and gpp_diff to have only the times of interest:
-gpp_diff <- gpp_diff[[ names(gpp_diff)[gpp_times %in% fmc_gpp_wc_times] ]]
-fmc_mean_diff <- fmc_mean_diff[[ names(fmc_mean_diff)[fmc_times %in% fmc_gpp_wc_times] ]]
+gpp_diff <- gpp_diff[[ names(gpp_diff)[gpp_times %in% out_dates] ]]
+fmc_mean_diff <- fmc_mean_diff[[ names(fmc_mean_diff)[fmc_times %in% out_dates] ]]
 
 # a simple brick of dates given model scaling
 dates <- gpp_diff
 names(dates) <- names(gpp_diff)
-values(dates) <- rep(prep_date(as.numeric(fmc_gpp_wc_times)), each = nrow(dates) * ncol(dates))
-
+values(dates) <- rep(prep_date(as.numeric(out_dates)), each = nrow(dates) * ncol(dates))
+names(dates) <- out_dates
 
 # interaction term
 gppwc_inter <- gpp_timemean * woodycover_smooth_extra_dates_stack
+
+# predictors combined nicely and exported:
 predictors_cannon_form = list(intercept = 1,
                   gpp_mean = gpp_timemean,
                   gpp_diff = gpp_diff,
@@ -123,14 +132,64 @@ predictors_cannon_form = list(intercept = 1,
                   woody_cover = woodycover_smooth_extra_dates_stack,
                   "gpp_mean:woody_cover" = gppwc_inter,
                   year = dates)
+mapply(function(x, y) writeRaster(x, filename = paste0("./tmpdata/predictors_cannon_form_", y, ".nc")),
+       predictors_cannon_form[2:6],
+       make.names(names(predictors_cannon_form)[2:6]),
+       SIMPLIFY = FALSE)
 
 
-#################### applying model prediction
+#################### applying model prediction #############
 boral_coefficients_matrix <- readRDS("./private/coefficients/boral_coefficients_matrix.rds")
 
 # weight predictors by coefficients and then sum
-out <- mapply("*", boral_coefficients_matrix[1, ], predictors_cannon_form, SIMPLIFY = FALSE)
-linearpred <- Reduce("+", out)
-predictedvalue <- calc(linearpred, pnorm)
-names(predictedvalue) <- fmc_gpp_wc_times
-plot(subset(predictedvalue, c("X2001.06.02", "X2001.12.19")))
+wght_sum_pnorm <- function(rowname, coefficient_mat, predictors_cannon_form){
+  coefficients <- coefficient_mat[rowname, ]
+  wght_pred <- mapply("*", coefficients, predictors_cannon_form, SIMPLIFY = FALSE)
+  linearpred <- Reduce("+", wght_pred)
+  prediction <- calc(linearpred, pnorm)
+  names(prediction) <- out_dates
+  writeRaster(prediction,
+              filename = paste0("./tmpdata/pred_", make.names(rowname), ".nc"),
+              varname = make.names(rowname),
+              longname = rowname,
+              zname = "time",
+              overwrite = TRUE)
+  return(prediction)
+}
+
+pred <- lapply(row.names(boral_coefficients_matrix), 
+              wght_sum_pnorm,
+              coefficient_mat = boral_coefficients_matrix,
+              predictors_cannon_form = predictors_cannon_form)
+names(pred) <- row.names(boral_coefficients_matrix)
+
+
+library(animation)
+#library(rasterVis)
+create_pred_gif <- function(x, name) {
+  saveGIF({
+    for (i in 1:nlayers(x)) plot(raster(x, i), zlim = c(0, 1), main = paste(name, names(x)[i]))
+    },
+    movie.name = paste0(make.names(name), ".gif"))
+}
+#create_pred_gif(pred[[1]], "Australian Raven")
+
+lapply(names(pred), function(x) create_pred_gif(pred[[x]], x))
+
+
+plot(pred[[1]], 5, zlim = c(0, 1))
+
+out <- animate(pred[[1]], pause = 0.00001)
+
+
+mapply(function(x, y) writeRaster(x,
+                                  filename = paste0("./tmpdata/pred_", make.names(y), ".nc"),
+                                  varname = make.names(y),
+                                  longname = y,
+                                  zname = "time",
+                                  overwrite = TRUE),
+       pred, names(pred))
+
+#### Plotting results ####
+plot(subset(predictedvalue, c("X2001.07.04", "X2002.01.01")))
+plot(subset(pred[[1]], c("X2001.07.04", "X2002.01.01")))
