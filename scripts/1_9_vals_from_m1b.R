0e42fbe4aab5f8ec900669edce041d06bb8f6ea7
Kassel Hingee <hingeek@fses.anu.edu.au>
2020-02-06 20:04:38 +1100

    draft script extracts full raster bricks when using m1b values

--- scripts/1_9_vals_from_m1b.R
+++ scripts/1_9_vals_from_m1b.R
@@ -1,82 +1,84 @@
 ## Preparation
-out <- lapply(c("tsibble", 'tidyr', 'dplyr', 'fable'),  #fable used for box_cox transformation fcnt
-       library, character.only = TRUE)
+invisible(lapply(c("sf", "sp", "raster", "maptools", "rgdal", "ncdf4", "lubridate",
+                   "tsibble", "tidyr", "dplyr", "fable"), #fable for the box_cox transformation
+       library, character.only = TRUE))
 out <- lapply(paste0("./functions/", list.files("./functions/")), source)
 
 m1b <- readRDS(file = "./private/models/m1b.rds")
 
-### load gpp and pg data, and convert to tsibbles
-load("./private/data/remote_sensed/pg_daily.Rdata")
-pg_daily$times <- as_date(pg_daily$times)
-pg <- pg_daily %>%
-  pivot_longer(-times, names_to = "site", values_to = "pg") %>%
-  as_tsibble(key = site, index = times)
-load("./private/data/remote_sensed/gpp_8d.Rdata")
-gpp <- gpp_8d %>%
-  pivot_longer(-times, names_to = "site", values_to = "gpp") %>%
-  as_tsibble(key = site, index = times)
-pggpp <- as_tsibble(dplyr::full_join(pg, gpp, by = c("times", "site")), key = site, index = times)
+# Construct Region Desired
+sws_sites <- readRDS("./private/data/clean/sws_sites.rds")
+points <- sws_sites_2_spdf(sws_sites)
+roi <- extent(points)
 
-### add time breakdowns
-pggpp <- pggpp %>%
-  mutate(yday = yday(times),
-         year = year(times))
+# build brick of pg data
+pg_brick <- brick_pg(points, 2001)
+gpp_brick <- brick_gpp(points, 2001)
+
+### Cumulative rainfalls
+pg_cum_brick <- calc(pg_brick, cumsum)
+pg_0to5m <- pg_cum_brick[[(1 + 5 * 31) : nlayers(pg_cum_brick)]] -  pg_cum_brick[[((1 + 5 * 31) : nlayers(pg_cum_brick) ) - 5*31]]
+names(pg_0to5m) <- names(pg_cum_brick[[(1 + 5 * 31) : nlayers(pg_cum_brick)]])
+pg_1to5m <- pg_0to5m[[-nlayers(pg_0to5m)]]
+names(pg_1to5m) <- names(pg_0to5m[[-1]])
+pg_24d <- pg_cum_brick[[(1 + 24) : nlayers(pg_cum_brick)]] -  pg_cum_brick[[((1 + 24) : nlayers(pg_cum_brick) ) - 24]]
+names(pg_24d) <- names(pg_cum_brick[[(1 + 24) : nlayers(pg_cum_brick)]])
+
+ydaymeds <- function(rasbrick){
+  dates <- as_date(names(rasbrick), format = "X%Y.%m.%d", tz = "")
+  ydays <- factor(yday(dates))
+  ydayidx <- as.numeric(factor(ydays))
+  medians <- stackApply(rasbrick, ydayidx, fun = median, na.rm = TRUE)
+  names(medians) <- gsub("index_", "X", levels(ydays)[ydayidx])
+  return(medians)
+}
+pg_1to5m.ydaymed <- ydaymeds(pg_1to5m)
+gpp.ydaymed <- ydaymeds(gpp_brick)
+
+m1b_predfordate <- function(date, pg_1to5m, pg_1to5m.ydaymed, pg_24d, gpp.ydaymed){
+  stopifnot(length(date) == 1)
+  datelayername <- strftime(date, format = "X%Y.%m.%d")
+  ydaylayername <- paste0("X", yday(date))
+  linpred <- m1b$coefficients[["(Intercept)"]] +
+          m1b$coefficients[["pg_1to5m"]] * pg_1to5m[[datelayername]] +
+          m1b$coefficients[["I(1/pg_1to5m.ydaymed)"]] * 1 / pg_1to5m.ydaymed[[ydaylayername]] +
+          m1b$coefficients[["pg_24d"]] * pg_24d[[datelayername]] +
+          m1b$coefficients[["pg_1to5m:I(1/pg_1to5m.ydaymed)"]] * pg_1to5m[[datelayername]] * 1 / pg_1to5m.ydaymed[[ydaylayername]] +
+          m1b$coefficients[["pg_1to5m:pg_24d"]] * pg_1to5m[[datelayername]] *  pg_24d[[datelayername]] +
+          m1b$coefficients[["I(1/pg_1to5m.ydaymed):pg_24d"]] * 1 / pg_1to5m.ydaymed[[ydaylayername]] * pg_24d[[datelayername]] +
+          m1b$coefficients[["pg_1to5m:I(1/pg_1to5m.ydaymed):pg_24d"]] * pg_1to5m[[datelayername]] * 1 / pg_1to5m.ydaymed[[ydaylayername]] * pg_24d[[datelayername]]
   
-### make sure sites are ordered alphabetically
-pggpp <- pggpp %>%
-  arrange(site) %>%
-  mutate(site = factor(site, ordered = TRUE))
+  
+  pred.gpp.ratio <- calc(linpred , function(x) fabletools::inv_box_cox(x, lambda = 0.1414141))
+  gpp.pred <- resample(gpp.ydaymed[[ydaylayername]], pred.gpp.ratio) * pred.gpp.ratio
+  return(gpp.pred)
+}
 
-### separate alpha part of site code
-pggpp <- pggpp %>%
-  mutate(farm = factor(substr(site, 1, 4)),
-         sitenum = factor(as.integer(substr(site, 5, 5))))
+dates <- as_date(names(gpp_brick), format = "X%Y.%m.%d", tz = "")[26:46]
+gpp.preds <- lapply(X = dates,
+                    FUN = function(x) m1b_predfordate(x , pg_1to5m, pg_1to5m.ydaymed, pg_24d, gpp.ydaymed))
+gpp.preds <- brick(gpp.preds)
+names(gpp.preds) <- dates
 
-
-### Add cumulative rainfalls
-pggpp <- pggpp %>% 
-  group_by_key() %>%
-  mutate(pg_cumsum = cumsum(pg)) %>%
-  mutate(pg_1to5m = lag(pg_cumsum, n = 1) - lag(pg_cumsum, n = 5*31)) %>%
-  mutate(pg_24d = pg_cumsum - lag(pg_cumsum, n = 3*8)) %>% 
-         # every 24 days (3*8) of GPP should be less correlated (given average GPP), this pg_24d corresponds to the rain through that period
-  ungroup()
+gpp.resids <- gpp_brick[[strftime(dates, format = "X%Y.%m.%d")]] - resample(gpp.preds, gpp_brick)
+# gpp.pred1 <- m1b_predfordate(as_date(names(gpp_brick), format = "X%Y.%m.%d", tz = "")[[40]],
+#                 pg_1to5m, pg_1to5m.ydaymed, pg_24d, gpp.ydaymed)
 
 
-### simple median of values
-ydaymedian <- pggpp %>%
-  filter(yday %in% seq(1, 366, by = 8)) %>%
-  group_by(site) %>%
-  index_by(yday) %>%
-  summarise(gpp.ydaymed = median(gpp),
-            pg_24d.ydaymed = median(pg_24d),
-            pg_1to5m.ydaymed = median(pg_1to5m, na.rm = TRUE))
-pggpp <- left_join(pggpp, ydaymedian, by = c("site", "yday"))
+## Convert above bricks into dataframes for the farm sites
+m1b_pred <- t(raster::extract(gpp.preds, points)) %>%
+            add_colnames_times_tseries(points$SiteCode)
 
-
-## Make predictions using m1b:
-pggpp$linpred <- predict.lm(m1b, newdata = pggpp)
-m1bout <- pggpp %>%
-  filter(yday(times) %in% seq(1, 366, by = 8)) %>%
-  mutate(gpp.pred = inv_box_cox(linpred, lambda = 0.1414141) * gpp.ydaymed) %>%
-  mutate(.resid = gpp - gpp.pred) %>%
-  dplyr::select(times, site, gpp.pred, .resid)
-
-
-m1b_pred <- m1bout %>%
-  filter(yday(times) %in% seq(1, 366, by = 8)) %>%
-  pivot_wider(id_cols = times, names_from = site, values_from = gpp.pred) %>%
-  as.data.frame()
-
-m1b_resid <- m1bout %>%
-  pivot_wider(id_cols = times, names_from = site, values_from = .resid) %>%
-  as.data.frame()
+m1b_resid <- t(raster::extract(gpp.resids, points)) %>%
+            add_colnames_times_tseries(points$SiteCode)
 
 ## Save Predictions and Residual
 saveRDS(m1b_pred, "./private/data/derived/m1b_pred.rds")
 saveRDS(m1b_resid, "./private/data/derived/m1b_resid.rds")
 
-# inner_join(m1bout, pggpp) %>%
+# m1b_pred %>%
+#   pivot_longer(-times, values_to = "gpp.pred", names_to = "site") %>%
+#   inner_join(pggpp, by = c("times", "site")) %>%
 #   filter(site == "ARCH1") %>%
 #   mutate(yday = yday(times)) %>%
 #   filter(yday %in% seq(1, 366, by = 8)) %>%
