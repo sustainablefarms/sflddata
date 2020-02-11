12badbd2778b9062f21071315573b8f50f3f960a
Kassel Hingee <hingeek@fses.anu.edu.au>
2020-02-11 14:23:35 +1100

    prediction from raster test still not working!

--- scripts/4_1_2_quick_prediction_script.R
+++ scripts/4_1_2_quick_prediction_script.R
@@ -43,13 +43,14 @@ predlyr <- wght_sum_pnorm(rowname = "Australasian Pipit",
 library(boral)
 preds <- readRDS("./private/predictions/4_1_boral_model_fitted_response.rds")
 ## Find out date that corresponds to Sept 6th
+testspecies <- "Grey Fantail"
 testvisits <- which( (month(preds$SurveyDate) == 9) & (day(preds$SurveyDate) %in% 1:12) )
-preds[testvisits, c("SurveyDate", "Australian Magpie")]  #all are in year 2011
-rasterpred_lyr <- wght_sum_pnorm("Australian Magpie", 
+preds[testvisits, c("SurveyDate", testspecies)]  #all are in year 2011
+rasterpred_lyr <- wght_sum_pnorm(testspecies, 
                                         2011,
                                         coefficient_mat = boral_coefficients_matrix,
                                         predictors_cannon_form = predictors_cannon_form)
 rasterpred_points <- raster::extract(rasterpred_lyr, preds[testvisits, c("longitude", "latitude")])
-cbind(preds[testvisits, c("SurveyDate", "Australian Magpie")], rasterpred_points)
-stopifnot(all.equal(preds[testvisits, "Australian Magpie"], rasterpred_points, tolerance = 0.1))
+cbind(preds[testvisits, c("SurveyDate", testspecies)], rasterpred_points)
+stopifnot(all.equal(preds[testvisits, testspecies], rasterpred_points, tolerance = 0.1))
 # These predictions *should* be close! But they aren't and there is even an NA value!!
