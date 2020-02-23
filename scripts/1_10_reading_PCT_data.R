9ff0e8a16168d1fab8b9af9c920141222d6e18bd
Kassel Hingee <kassel.hingee@gmail.com>
2020-02-24 10:31:27 +1100

    draft of using readOGR to open PCT file

---
+++ scripts/1_10_reading_PCT_data.R
@@ -0,0 +1,25 @@
+# reading PCT data test
+library(rgdal)
+library(raster)
+library(sf)
+
+source("./functions/sites_2_sp_points.R")
+sws_sites <- readRDS("./private/data/clean/sws_sites.rds")
+points <- sws_sites_2_sf(sws_sites)
+
+gdb <- "../LargeInputData/vegetationsvtmriverinav1.2quickviews4469/SVTM_Riverina_v1p2.gdb"
+layers <- ogrListLayers(gdb)
+boundary <- st_as_sf(readOGR(gdb, layer = "Riverina_v1p2_Boundary"))
+boundary <- st_transform(boundary, st_crs(points))
+
+sheetboundary <- st_as_sf(readOGR(gdb, layer = "Riverina_100k_Sheet_Boundaries"))
+sheetboundary <- st_transform(sheetboundary, st_crs(points))
+
+IDtable <- st_read(gdb, layer = "PCT_Structural_Riverina_LUT_v1p2") #table of PCT IDs and corresponding names, Keith class and Keith form
+
+
+boundary %>%
+  ggplot() +
+  geom_sf() +
+  geom_sf(data = sheetboundary, fill = NA) +
+  geom_sf(data = points)
