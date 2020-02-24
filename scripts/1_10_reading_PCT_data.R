da704cd76f1f72427cfb5371682003231495984c
Kassel Hingee <hingeek@fses.anu.edu.au>
2020-02-24 11:54:58 +1100

    further attempts to view PCT data

--- scripts/1_10_reading_PCT_data.R
+++ scripts/1_10_reading_PCT_data.R
@@ -8,18 +8,27 @@ sws_sites <- readRDS("./private/data/clean/sws_sites.rds")
 points <- sws_sites_2_sf(sws_sites)
 
 gdb <- "../LargeInputData/vegetationsvtmriverinav1.2quickviews4469/SVTM_Riverina_v1p2.gdb"
-layers <- ogrListLayers(gdb)
+layers <- st_layers(gdb)
+
+IDtable <- st_read(gdb, layer = "PCT_Structural_Riverina_LUT_v1p2") #table of PCT IDs and corresponding names, Keith class and Keith form
+
+PCTsurveysites <- st_transform(st_read(gdb, layer = "Riverina_v1p2_SurveySites"), st_crs(points))
+
 boundary <- st_as_sf(readOGR(gdb, layer = "Riverina_v1p2_Boundary"))
 boundary <- st_transform(boundary, st_crs(points))
 
 sheetboundary <- st_as_sf(readOGR(gdb, layer = "Riverina_100k_Sheet_Boundaries"))
 sheetboundary <- st_transform(sheetboundary, st_crs(points))
 
-IDtable <- st_read(gdb, layer = "PCT_Structural_Riverina_LUT_v1p2") #table of PCT IDs and corresponding names, Keith class and Keith form
+unknown <- st_transform(st_read(gdb, layer = "SVTM_Riverina_v1p2_5m"), st_crs(points)) #what is this?
+
+quickview <- st_transform(st_read(gdb, layer = "Riverina_v1p2_Quickview"), st_crs(points))
 
 
 boundary %>%
   ggplot() +
   geom_sf() +
+  geom_sf(data = quickview) +
   geom_sf(data = sheetboundary, fill = NA) +
+  geom_sf(data = PCTsurveysites, col = "green", pch = "+") +
   geom_sf(data = points)
