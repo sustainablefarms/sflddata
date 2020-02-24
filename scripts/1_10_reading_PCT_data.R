ae57127720fca368fccb4b1e60143b01a7c98541
Kassel Hingee <kassel.hingee@gmail.com>
2020-02-24 15:21:31 +1100

    recorded attempt to read PCT data

--- scripts/1_10_reading_PCT_data.R
+++ scripts/1_10_reading_PCT_data.R
@@ -7,7 +7,13 @@ source("./functions/sites_2_sp_points.R")
 sws_sites <- readRDS("./private/data/clean/sws_sites.rds")
 points <- sws_sites_2_sf(sws_sites)
 
+extent(points)
+# ogr2ogr -clipsrc 146.6404 -35.93073 148.1461 -34.64599  -f "ESRI Shapefile" SVTM_Riverina_v1p2B.gdb   SVTM_Riverina_v1p2.gdb
+# Above use of ogr2ogr created errors. It might be that the best way to use the map is via a rasterisation
+
+
 gdb <- "../LargeInputData/vegetationsvtmriverinav1.2quickviews4469/SVTM_Riverina_v1p2.gdb"
+gdb <- "/media/kassel/KassTraveler/vegetationsvtmriverinav1.2quickviews4469/SVTM_Riverina_v1p2.gdb"
 layers <- st_layers(gdb)
 
 IDtable <- st_read(gdb, layer = "PCT_Structural_Riverina_LUT_v1p2") #table of PCT IDs and corresponding names, Keith class and Keith form
