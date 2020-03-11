040ec60a95d80f3e932cf317af0d5950ecb524c0
Kassel Hingee <hingeek@fses.anu.edu.au>
2020-03-11 11:50:51 +1100

    polishing of the SQL extraction functions

--- functions/SF_sql_reading.R
+++ functions/SF_sql_reading.R
@@ -6,26 +6,26 @@
 #' @param con A connection to the SQL database. Created, for example, by `DBI::dbConnect`
 #' @param last Column names to put last when querying the database
 #' @param tryperm TRUE means try different permutations of the column ordering until table successfully extracted.
+#' @param verbose If TRUE prints each permutation of column names before it is tried.
 #' @importFrom arrangements ipermutations
+#' @importFrom odbc dbGetQuery
 library(arrangements)
 
-getSFtable <- function(tablename, schema, con, last = NULL, tryperm = TRUE){
+getSFtable <- function(tablename, schema, con, last = NULL, tryperm = TRUE, verbose = tryperm){
   # get column names for tables
   allcols <- dbGetQuery(con, "SELECT * FROM INFORMATION_SCHEMA.COLUMNS")
-  cols <- allcols %>% 
-    dplyr::filter(TABLE_NAME == tablename,
-                  TABLE_SCHEMA == schema) %>%
-    dplyr::select(COLUMN_NAME)
+  cols <- allcols[(allcols$TABLE_NAME == tablename) & (allcols$TABLE_SCHEMA == schema),
+                  "COLUMN_NAME"]
   
   # put last columns last
   cols_ro <- c(setdiff(cols[, 1], last), last)
   
   out <- NULL
-  if (tryperm){ 
+  if (tryperm){  #try extractions
     iperm <- ipermutations(cols_ro, replace = FALSE)
     while(is.null(out)) {
       cols_ro2 <- cols_ro[iperm$getnext(1)]
-      print(paste("Trying ", paste(cols_ro2, collapse = ", ")))
+      if (verbose) { print(paste("Trying ", paste(cols_ro2, collapse = ", "))) }
       tryCatch(out <- colsfromtbl(cols_ro2, tablename, schema, con),
                error = function(e) {
                  if(!grepl("Invalid Descriptor Index", e)){stop(e)}
@@ -33,9 +33,9 @@ getSFtable <- function(tablename, schema, con, last = NULL, tryperm = TRUE){
                warning = function(w) w
       )
     }
-    print("Success.")
+    if (verbose) {print("Success.")}
   }
-  else {
+  else {# just go with reordered extractions
     out <- colsfromtbl(cols_ro, tablename, schema, con)
   }
   
@@ -44,7 +44,7 @@ getSFtable <- function(tablename, schema, con, last = NULL, tryperm = TRUE){
   return(out[, cols[, 1]])
 }
 
-
+#' @describeIn getSFtable Extract specific columns from a table in the SQL database
 colsfromtbl <- function(cols, tablename, schema, con){
   # make table+schema string name
   tblescheme <- paste0("[", schema, "].[", tablename, "]")
@@ -54,48 +54,4 @@ colsfromtbl <- function(cols, tablename, schema, con){
                        tblescheme)
   out <- dbGetQuery(con, sqlquerystr)
   return(out)
-}
-
-getSFtable_last <- function(tablename, schema, con, last = NULL){
-  # get column names for tables
-  allcols <- dbGetQuery(con, "SELECT * FROM INFORMATION_SCHEMA.COLUMNS")
-  cols <- allcols %>% 
-    dplyr::filter(TABLE_NAME == tablename,
-                  TABLE_SCHEMA == schema) %>%
-    dplyr::select(COLUMN_NAME)
- 
-  # put VisitCode last
-  cols_ro <- c(setdiff(cols[, 1], last), last)
-  out <- colsfromtbl(cols_ro, tablename, schema, con)
-  
-  #order output columns in same order as in SCHEMA
-  return(out[, cols[, 1]])
-}
-
-
-getSFtable_permorder <- function(tablename, schema, con){
-  # get column names for tables
-  allcols <- dbGetQuery(con, "SELECT * FROM INFORMATION_SCHEMA.COLUMNS")
-  cols <- allcols %>% 
-    dplyr::filter(TABLE_NAME == tablename,
-                  TABLE_SCHEMA == schema) %>%
-    dplyr::select(COLUMN_NAME)
-  
-  # try pulling out table by permuting order
-  out <- NULL
-  iperm <- ipermutations(cols[, 1], replace = FALSE)
-  while(is.null(out)) {
-    cols_ro <- cols[iperm$getnext(1), ]
-    print(paste("Trying ", paste(cols_ro, collapse = ", ")))
-    tryCatch(out <- colsfromtbl(cols_ro, tablename, schema, con),
-             error = function(e) {
-               if(!grepl("Invalid Descriptor Index", e)){stop(e)}
-             },
-             warning = function(w) w
-    )
-  }
-  print("Success.")
-  
-  #order output columns correctly
-  return(out[, cols[, 1]])
 }
