4d1de18a798d005c9295d8a3c3a16096d3a7fe05
Kassel Hingee <kassel.hingee@gmail.com>
2020-03-23 16:03:40 +1100

    alterations to simplify into the same detection covariates per site - consistent with Tobler for initial experimentation.

--- scripts/7_1_import_site_observations.R
+++ scripts/7_1_import_site_observations.R
@@ -10,7 +10,7 @@ birds_raw <- as.data.frame(
 
 #### Notes on this data
 # for each survey vist (SurveyVisitId), no species is recorded twice
-# each SiteCode has 1+ visits (called RepeatVisits 1, 2 ...)
+# each SiteCode has 1+ visits (called RepeatVisits 1, 2 ...) per year
 # each visit typically has 3 plots
 # each visit to each plot is considered a different 'SurveyVisitId'
 # abundance of each species noticed could be recorded in 'SumAbundance'?
@@ -26,6 +26,19 @@ birds_wider <- birds_raw %>%
 
 # birds_wider$SurveyVisitId should equal the unique SurveyVisitId
 stopifnot(all.equal(unique(birds_raw$SurveyVisitId), birds_wider$SurveyVisitId))
+
+# check that number of plots in each transect is equal (equal effort)
+birds_wider %>%
+  group_by(SurveyYear, SiteCode, SurveySeasonId, RepeatNumber) %>%
+  summarise(numplts = n(), maxplotnum = max(PlotNumber)) %>%
+  mutate(correctamtplts = (numplts == 3), correctpltsnumber = (maxplotnum == 3)) %>%
+  ungroup() %>%
+  select(numplts, maxplotnum) %>%
+  summarise_all(all) %>%
+  unlist() %>%
+  all() %>%
+  stopifnot()
+# will stop if there are any transects with different to 3 visits, or plot numbers that don't have a maximum of 3.
 
 species <- sort(unique(birds_raw$CommonName))
 
@@ -72,5 +85,22 @@ birds_clean <- birds_clean %>%
 
 
 ########################################################
-detection_data <- birds_clean
+## Simplify for test Tobler run: combine visits each year into a tally, reduce transect points to detected or not too.
+detections <- birds_clean %>%
+  group_by(SurveyYear, SiteCode, SurveySeasonId, RepeatNumber) %>%
+  summarise_at(.vars = vars(matches(species)), max) %>%   #detection simplified to binary per transect
+  summarise_at(vars(matches(species)), sum)  # sum across visits
+
+simplifiedcovars <- birds_clean %>%
+  group_by(SurveyYear, SiteCode, SurveySeasonId, RepeatNumber) %>%
+  summarise(MeanWind = mean(WindId),
+            MeanTime = mean(SurveyStartMinutesSinceMidnight)) %>% #summary for each transect
+  summarise(NumVisits = n(),
+            MeanWind = mean(MeanWind),
+            MeanTime = mean(MeanTime))
+
+birds_clean_aggregated <- inner_join(simplifiedcovars, detections)
+
+########################################################
+detection_data <- birds_clean_aggregated
 ########################################################
