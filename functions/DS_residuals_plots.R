b1d1b90aeb387e4adaa6b268581f67ba897a5785
Kassel Hingee <kassel.hingee@gmail.com>
2020-05-22 10:45:53 +1000

    combined the code for the plotting functions for better polishing

--- functions/DS_residuals_plots.R
+++ functions/DS_residuals_plots.R
@@ -10,22 +10,22 @@ library(ggplot2); library(dplyr);
 #' fit <- readRDS("./tmpdata/7_1_mcmcchain_20200424.rds")
 #' fit <- runjags::add.summary(fit)
 #' source("./functions/calcpredictions.R")
-#' source("./functions/DS_residuals")
+#' source("./functions/DS_residuals.R")
 #' detection_resids <- ds_detection_residuals.fit(fit, type = "median")
-#' plot_detection_residual.fit(fit, detection_resids, varidx = 2)
-#' plot_detection_residual.fit(fit, varidx = 2, esttype = "median")
-#' plot_detection_residual.fit(fit, varidx = 4, esttype = "median")
+#' plot_residuals_detection.fit(fit, detection_resids, varidx = 2)
+#' plot_residuals_detection.fit(fit, varidx = 2, esttype = "median")
+#' plot_residuals_detection.fit(fit, varidx = 4, esttype = "median")
 #' 
-#' plot_occupancy_residual.fit(fit, varidx = 3, esttype = "median")
-#' plot_occupancy_residual.fit(fit, varidx = c(2, 3, 4), esttype = "median")
+#' plot_residuals_occupancy.fit(fit, varidx = 3, esttype = "median")
+#' plot_residuals_occupancy.fit(fit, varidx = c(2, 3, 4), esttype = "median")
 #' 
-#' # To an unincluded covariate:
+#' # Residuals against an unincluded covariate:
 #' source("./scripts/7_1_import_site_observations.R")
-# covar <- occ_covariates[ , "ms_cover", drop = FALSE] %>%
-#   rowid_to_column(var = "ModelSite")
-# residuals <- ds_occupancy_residuals.fit(fit, type = "median")
-# pltobj <- plot_residuals.residual(residuals, covar, plot = FALSE)
-# pltobj + scale_y_continuous(name = "Occupancy Residuals")
+#' covar <- occ_covariates[ , "ms_cover", drop = FALSE] %>%
+#'   rowid_to_column(var = "ModelSite")
+#' residuals <- ds_occupancy_residuals.fit(fit, type = "median")
+#' pltobj <- plot_residuals.residual(residuals, covar, plot = FALSE)
+#' pltobj + scale_y_continuous(name = "Occupancy Residuals")
 
 #' @describeIn plot_residuals For table of provided residuals and covariate values, makes residual plots.
 #' Residual and covariate values must be provided with the ModelSite.
@@ -34,9 +34,10 @@ library(ggplot2); library(dplyr);
 #' @param covar Is a table of covariate values, it must have a column labelled 'ModelSite' that gives the ModelSite of covariate value.
 #' Rows with duplicated ModelSite values are averaged, which is in keeping with Warton et al for detection residuals.
 plot_residuals.residual <- function(residuals, covar, plot = TRUE){
-  # Average to ModelSite level
-  if (anyDuplicated(covar$ModelSite) > 0){
+  # Average covariates to ModelSite level. This is what Warton, Mackenzie et al do. In future it could be possible to present residuals per visit
+  if (anyDuplicated(covar[, "ModelSite"]) > 0){
     covar <- covar %>%
+      as_tibble() %>%
       group_by(ModelSite) %>%
       summarise_all(mean)
   }
@@ -81,42 +82,18 @@ plot_residuals.residual <- function(residuals, covar, plot = TRUE){
 #' @param esttype The point estimate extracted from fit. Passed to \code{ds_detection_residuals.fit} as argument \code{type}.
 #' @param plot Defaults to TRUE. If false return the ggplot object without plotting it.
 #' @value A ggplot object. Data is saved in the \code{data} slot.
-plot_detection_residual.fit <- function(fit, detectionresiduals = NULL, varidx = NULL, esttype = NULL, plot = TRUE){
+plot_residuals_detection.fit <- function(fit, detectionresiduals = NULL, varidx = NULL, esttype = NULL, plot = TRUE){
   stopifnot(is.null(detectionresiduals) | is.null(esttype))  #error if est type is supplied when detection residuals is also supplied
   fitdata <- list.format(fit$data)
   if (is.null(detectionresiduals)) {detectionresiduals <- ds_detection_residuals.fit(fit, type = esttype)}
-  speciesnames <- setdiff(names(detectionresiduals), "ModelSite")
-  speciesnames <- speciesnames[order(as.integer(gsub("^S", "", speciesnames)))]  #order the species by index
   
   # get detection covariates
   detectioncovars <- fitdata$Xobs
   colnames(detectioncovars)[1:fitdata$Vobs] <- paste0("Xobs", 1:fitdata$Vobs)
   if (!is.null(varidx)){detectioncovars <- detectioncovars[, varidx, drop = FALSE]}  
   detectioncovars <- cbind(detectioncovars, ModelSite = fitdata$ObservedSite)
-  
-  # Average detection covariates to ModelSite level. This is what Warton, Mackenzie et al do. In future it could be possible to present residuals per visit
-  detectioncovars_ModelSite <- detectioncovars %>%
-    as_tibble() %>%
-    group_by(ModelSite) %>%
-    summarise_all(mean)
-  covarnames <- setdiff(names(detectioncovars_ModelSite), "ModelSite")
-  
-  outframe <- left_join(detectionresiduals, detectioncovars_ModelSite, by = "ModelSite") %>%
-    pivot_longer(any_of(speciesnames),
-               names_to = "Species",
-               values_to = "Residual",
-               values_drop_na = TRUE) %>%
-    mutate(Species = ordered(Species, levels = speciesnames)) #to order species according to 'speciesnames'
-  outframe <- outframe %>%
-    pivot_longer(any_of(covarnames),
-                 names_to = "Covariate",
-                 values_to = "CovarValue") 
-  pltobject <- outframe %>% 
-      ggplot() +
-      facet_wrap(~ Covariate + Species, as.table = TRUE) +
-      geom_hline(yintercept = 0, col = "grey", lty = "dashed") +
-      geom_point(aes(x = CovarValue, y = Residual)) +
-      geom_smooth(aes(x = CovarValue, y = Residual), method = "gam", level = 0.95, formula = y ~ s(x, bs = "cs")) +
+ 
+  pltobject <- plot_residuals.residual(detectionresiduals, detectioncovars, plot = FALSE) +
       scale_y_continuous(name = "Detection Residual")
   if (plot){
     print(pltobject)
@@ -133,12 +110,10 @@ plot_detection_residual.fit <- function(fit, detectionresiduals = NULL, varidx =
 #' @param esttype The point estimate extracted from fit. Passed to \code{ds_detection_residuals.fit} as argument \code{type}.
 #' @param plot Defaults to TRUE. If false return the ggplot object without plotting it.
 #' @value A ggplot object. Data is saved in the \code{data} slot.
-plot_occupancy_residual.fit <- function(fit, occupancyresidual = NULL, varidx = NULL, esttype = NULL, plot = TRUE){
+plot_residuals_occupancy.fit <- function(fit, occupancyresidual = NULL, varidx = NULL, esttype = NULL, plot = TRUE){
   stopifnot(is.null(occupancyresidual) | is.null(esttype))  #error if est type is supplied when detection residuals is also supplied
   fitdata <- list.format(fit$data)
   if (is.null(occupancyresidual)) {occupancyresidual <- ds_occupancy_residuals.fit(fit, type = esttype)}
-  speciesnames <- setdiff(names(occupancyresidual), "ModelSite")
-  speciesnames <- speciesnames[order(as.integer(gsub("^S", "", speciesnames)))]  #order the species by index
   
   # get occupancy covariates
   occupancycovars <- fitdata$Xocc
@@ -146,42 +121,10 @@ plot_occupancy_residual.fit <- function(fit, occupancyresidual = NULL, varidx = 
   if (!is.null(varidx)){occupancycovars <- occupancycovars[, varidx, drop = FALSE]}  
   occupancycovars <- occupancycovars %>% as_tibble() %>% rowid_to_column(var = "ModelSite")
   
-  # Average detection covariates to ModelSite level. This is what Warton, Mackenzie et al do. In future it could be possible to present residuals per visit
-  occupancycovars_ModelSite <- occupancycovars %>%
-    as_tibble() %>%
-    group_by(ModelSite) %>%
-    summarise_all(mean)
-  covarnames <- setdiff(names(occupancycovars_ModelSite), "ModelSite")
-  
-  outframe <- left_join(occupancyresidual, occupancycovars_ModelSite, by = "ModelSite") %>%
-    pivot_longer(any_of(speciesnames),
-               names_to = "Species",
-               values_to = "Residual",
-               values_drop_na = TRUE) %>%
-    mutate(Species = ordered(Species, levels = speciesnames)) #to order species according to 'speciesnames'
-  outframe <- outframe %>%
-    pivot_longer(any_of(covarnames),
-                 names_to = "Covariate",
-                 values_to = "CovarValue") 
-  pltobject <- outframe %>% 
-      ggplot() +
-      facet_wrap(~ Covariate + Species, as.table = TRUE) +
-      geom_hline(yintercept = 0, col = "grey", lty = "dashed") +
-      geom_point(aes(x = CovarValue, y = Residual)) +
-      geom_smooth(aes(x = CovarValue, y = Residual), method = "gam", level = 0.95, formula = y ~ s(x, bs = "cs")) +
+  pltobject <- plot_residuals.residual(occupancyresidual, occupancycovars, plot = FALSE) +
       scale_y_continuous(name = "Occupancy Residual")
   if (plot){
     print(pltobject)
   }
   invisible(pltobject)
-}
-
-#' @param residualtable is a dataframe with first column ModelSite, and second column the residual (this works for both occupancy and detection residuals)
-#' @param covariatetable is a dataframe with first column ModelSite and other column's covariate values.
-plot.dsresidual <- function(residualtable, covariatetable){
-  
-}
-
-prep_residualdf <- function(residualtable, covariatetable){
-  
 }
