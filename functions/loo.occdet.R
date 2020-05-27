561f292172034257b1f16594ba2aff37fab00043
Kassel Hingee <kassel.hingee@anu.edu.au>
2020-05-28 08:49:34 +1000

    more corrections to cluster apply use

--- functions/loo.occdet.R
+++ functions/loo.occdet.R
@@ -55,27 +55,32 @@
 #' numsims <- 1000
 #' lvsim <- matrix(rnorm(nlv * numsims), ncol = 2, nrow = numsims) #simulated lv values, should average over thousands
 #' draws <- fit$mcmc[[1]]
-#' pdetect_joint_marginal.data_i(data_i = data[1, , drop = FALSE], draws[1:5, ], lvsim, cores4LL = 3)
+#' cl <- parallel::makeCluster(5)
+#' pdetect_joint_marginal.data_i(data_i = data[1, , drop = FALSE], draws[1:5, ], lvsim, cl = cl)
 
 #' library(loo)
 #' waic <- loo::waic(pdetect_joint_marginal.data_i,
 #'                   data = data,
 #'                   draws = draws,
 #'                   lvsim = lvsim,
-#'                   cores4LL = 5)
-#' looest <- loo::loo(pdetect_joint_marginal.data_i, data = data, draws = draws[1:5, ], lvsim = lvsim, cores4LL = 5)
-
+#'                   cl = cl)
+#' looest <- loo::loo(pdetect_joint_marginal.data_i,
+#'                    data = data,
+#'                    draws = draws,
+#'                    lvsim = lvsim,
+#'                    cl = cl)
+#' parallel::stopCluster(cl)
 
 #' @param draws A large matrix. Each column is a model parameter, with array elements named according to the BUGS naming convention.
 #' Each row of \code{draws} is a simulation from the posterior.
 #' @param data_i A row of a data frame containing data for a single ModelSite. 
 #' @param lvsim A matrix of simulated LV values. Columns correspond to latent variables, each row is a simulation
-pdetect_joint_marginal.data_i <- function(data_i, draws, lvsim, cores4LL = 2){
+pdetect_joint_marginal.data_i <- function(data_i, draws, lvsim, cl = NULL){
   Xocc <- data_i[, "Xocc", drop = TRUE][[1]]
   Xobs <- data_i[, "Xobs", drop = TRUE][[1]]
   y <- data_i[, "y", drop = TRUE][[1]]
   
-  cl <- parallel::makeCluster(cores4LL)
+  if (is.null(cl)){cl <- parallel::makeCluster()}
   parallel::clusterExport(cl, list("pdetect_joint_marginal.ModelSite",
                          "JointSpVst_Liklhood.LV",
                          "bugsvar2array",
@@ -87,8 +92,7 @@ pdetect_joint_marginal.data_i <- function(data_i, draws, lvsim, cores4LL = 2){
   # drawrows <- lapply(drawrows, setNames, nm = colnames(draws))
   Likl_margLV <- parallel::parApply(cl, draws, 1, function(theta) pdetect_joint_marginal.ModelSite(
     Xocc, Xobs, y, theta, lvsim))
-  stopCluster(cl)
-  return(log(unlist(Likl_margLV)))
+  return(log(Likl_margLV))
 }
 
 #' @param Xocc A matrix of occupancy covariates. Must have a single row. Columns correspond to covariates.
