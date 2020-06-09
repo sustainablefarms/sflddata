de2d5e706b350177178ea1ee0c166bb245db2970
Kassel Hingee <kassel.hingee@gmail.com>
2020-06-09 15:42:22 +1000

    function for creating artificial fit objects

--- functions/simulate_fit.R
+++ functions/simulate_fit.R
@@ -44,3 +44,77 @@ simulate.fit <- function(fit, esttype = "median", conditionalLV = TRUE){
   
   return(detected)
 }
+
+## function for creating a fully artificial fitted object
+#' @value A list that has enough similarities to runjags objects that residual calculations are possible.
+#' The true parameter set is the first (and only row) of the first MCMC chain.
+#' It can be accessed using get_theta(fit, type = 1)
+#' @param nspecies Number of species
+#' @param nsites Number of sites
+#' @param nvisitspersite Number of visits per site
+#' @param nlv Number of latent variables. Must be 4 or less
+#' @param OccFmla Formula for occupancy. Available variables: UpSite, Sine1 and Sine2
+#' @param ObsFmla Formula for detection. Available variables: Upvisit, Step
+#' @param u.b.min, u.b.max, v.b.min, v.b.max The upper and lower bouonds of the u.b and v.b parameters.
+#'  May be a single number or an array with rows corresponding to species and columns to covariates.
+#'  @examples 
+#'  artfit <- artificial_runjags(nspecies = 2, nsites = 10, nvisitspersite = 4, nlv = 2)
+artificial_runjags <- function(nspecies = 4, nsites = 100, nvisitspersite  = 2, nlv = 2,
+                               OccFmla = "~ UpSite + Sine1 + Sine2",
+                               ObsFmla = "~ UpVisit + Step",
+                               u.b.min = -1,
+                               u.b.max = 1,
+                               v.b.min = -1,
+                               v.b.max = 1
+                               ){
+  species <- LETTERS[1:nspecies]
+  sites <- c(1:nsites)
+  XoccIn <- data.frame(ModelSite = sites,
+                       UpSite = sites,
+                       Sine1 = sin(2 * pi * sites / nsites),
+                       Sine2 = sin(4 * pi * sites / nsites))
+  XobsIn <- data.frame(ModelSite = rep(sites, nvisitspersite),
+                       UpVisit = 1:(nvisitspersite*nsites),
+                       Step = c(rep(0, floor(nvisitspersite*nsites / 2)),
+                                rep(1, ceiling(nvisitspersite*nsites / 2)))
+                       )
+  LV <- scale(cbind(sites %% 2,
+                    ((sites %/% 5) * 5 == sites ) | (sites %/% 3) * 3 == sites,
+              sites < (nsites / 2), 
+              sites > (nsites / 5)))
+  LV <- LV[, 1:nlv]
+  OccFmla <- "~ UpSite + Sine1 + Sine2"
+  ObsFmla <- "~ UpVisit + Step"
+  XoccProcess <- prep.designmatprocess(XoccIn, OccFmla)
+  XobsProcess <- prep.designmatprocess(XobsIn, ObsFmla)
+
+  data.list <- prep.data(XoccIn, yXobs = XobsIn,
+                         ModelSite = "ModelSite", 
+                         species = NULL,
+                         nlv = 2, 
+                         XoccProcess = XoccProcess,
+                         XobsProcess = XobsProcess)
+  fit <- list()
+  fit$data <- data.list
+  fit$data$n <- length(species)
+  fit$species <- species
+  fit$summary.available <- TRUE
+
+  # set parameters
+  u.b <- matrix(runif( fit$data$n * fit$data$Vocc, min = u.b.min, max = u.b.max), nrow = fit$data$n, ncol = fit$data$Vocc, byrow = FALSE)
+  v.b <- matrix(runif(  fit$data$n * fit$data$Vobs, min = v.b.min, max = v.b.max), nrow = fit$data$n, ncol = fit$data$Vobs, byrow = FALSE)
+  set.seed(324)
+  lv.coef <- matrix(runif(  fit$data$n * fit$data$nlv, min = -0.5, max = 0.5), nrow = fit$data$n, ncol = fit$data$nlv) #0.5 constraint makes sure rowSum(lv.coef^2) < 1
+  theta <- c(matrix2bugsvar(u.b, name = "u.b"),
+             matrix2bugsvar(v.b, name = "v.b"),
+             matrix2bugsvar(lv.coef, name = "lv.coef"),
+             matrix2bugsvar(LV, name = "LV")
+  )
+  fit$mcmc <- list()
+  fit$mcmc[[1]] <- t(as.matrix(theta))
+
+  # simulate data
+  fit$data$y <- simulate.fit(fit, esttype = 1, conditionalLV = TRUE)
+  colnames(fit$data$y) <- species
+  return(fit)
+}
