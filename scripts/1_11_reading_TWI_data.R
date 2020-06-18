d7dd12437b4a3cf887db6458883ce8c5201b0829
Kassel Hingee <kassel.hingee@gmail.com>
2020-06-18 13:04:50 +1000

    twi reading updated

--- scripts/1_11_reading_TWI_data.R
+++ scripts/1_11_reading_TWI_data.R
@@ -1,32 +1,30 @@
 # Extract TWI
 # data described here: https://www.clw.csiro.au/aclep/soilandlandscapegrid/ProductDetails-LandscapeAttributes.html
-library(raster);library(maptools);library(rgdal);library(ncdf4);library(lubridate);library(dplyr);
 library(slga) #specialist library for reading the data
-source("./R/sites_2_sp_points.R")
+library(sf)
+locs <- read.csv("private/data/clean/site_locations_cleaned.csv", stringsAsFactors = FALSE)
+locs_wgs84 <- st_as_sf(locs, coords = c("MeanLON", "MeanLAT"))
+locs_wgs84 <- st_set_crs(locs_wgs84, 4326) #4326 is the epsg code for WGS84, make this default crs I'll return to
 
-# get spatial points
-source("./R/sites_2_sp_points.R")
-sws_sites <- readRDS("./private/data/clean/sws_sites.rds")
-points <- sws_sites_2_sf(sws_sites)
+twiprodcode <- slga_product_info %>%
+  filter(Product == "Topographic Wetness Index") %>%
+  select(Short_Name) %>%
+  as.character()
 
-# TWI at points
-get_lscape_point(slga_product_info %>%
-                   filter(Product == "Topographic Wetness Index") %>%
-                   select(Short_Name) %>%
-                   as.character(),
-                 c(148.0779, -35.13167))
-get_lscape_point(slga_product_info %>%
-                   filter(Product == "Topographic Wetness Index") %>%
-                   select(Short_Name) %>%
-                   as.character(),
-                   points[1, ])
+twis <- lapply(1:nrow(locs_wgs84), function(id) {
+  get_lscape_point(twiprodcode,
+                     poi = locs_wgs84[id, ])
+  })
+twis <- unlist(twis)
+twidf <- data.frame(TWI = twis, SiteCode = locs_wgs84$SiteCode)
+saveRDS(twidf, "./private/data/raw/TWI_boxgum_sites.rds")
+
 
 # TWI for whole region
-twi_ras <- get_lscape_data(slga_product_info %>%
-                   filter(Product == "Topographic Wetness Index") %>%
-                   select(Short_Name) %>%
-                   as.character(),
-                   aoi = points)
-
-#twi at the points
-twi_pts <- extract(twi_ras, points)
+# twi_ras <- get_lscape_data(slga_product_info %>%
+#                    filter(Product == "Topographic Wetness Index") %>%
+#                    select(Short_Name) %>%
+#                    as.character(),
+#                    aoi = locs_wgs84[locs_wgs84$StudyCode == "Nanangroe Natural Experiment", ])
+# plot(twi_ras)
+# plot(locs_wgs84, add = TRUE)
