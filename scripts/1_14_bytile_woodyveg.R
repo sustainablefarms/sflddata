1344f29db6d777fa48c6b7be834a487f14bdab46
Kassel Hingee <kassel.hingee@gmail.com>
2020-06-19 08:13:00 +1000

    woodyveg download script has error catching and reattempts

--- scripts/1_14_bytile_woodyveg.R
+++ scripts/1_14_bytile_woodyveg.R
@@ -8,25 +8,40 @@ locs_wgs84 <- st_set_crs(locs_wgs84, 4326) #4326 is the epsg code for WGS84, mak
 # locs_wgs84 %>%
 #   filter(StudyCode == "Restoration Study") %>%
 #   st_bbox()
-tileswpts <- divide_into_tiles(locs_wgs84, cellsize = 0.05, buffer = 0.01)
+# tileswpts <- divide_into_tiles(locs_wgs84, cellsize = 0.05, buffer = 0.01)
+tileswpts <- readRDS("./tmpdata/woodyl_tileswpts.rds")
 tiles <- lapply(tileswpts, function(x) st_as_sf(x$tile))
 tilegrd <- do.call(rbind, tiles)
 # plot(tilegrd)
 
-length(tileswpts)
+#### read RS rasters ####
+# empty gppl object
+woodyl <- lapply(tileswpts, function(x) return(NULL))
+woodyl <- readRDS("./tmpdata/woodyl.rds")
+uncompleted <- vapply(woodyl, is.null, FUN.VALUE = FALSE)
+attempts <- 0
+
+rasterOptions(tmpdir = "/media/kassel/Seagate1TB/tmpdir/")
+while(any(uncompleted) && attempts <= 5){
+  cat("Attempting:", sum(uncompleted), "tiles.\n")
+  woodyl[uncompleted] <- pbapply::pblapply(tileswpts[uncompleted],
+                                         FUN = function(x) {
+                                           woody <- NULL
+                                           try(woody <- woody_vals_buffer(x$tile, x$pts, 2000:2018, 500))
+                                           gc()
+                                           return(woody)
+                                         })
+  uncompleted <- vapply(woodyl, is.null, FUN.VALUE = FALSE)
+  attempts <- attempts + 1
+}
 
 # tile <- tileswpts[[1]]$tile
 # pts <- tileswpts[[1]]$pts
 
-woodyl <- pbapply::pblapply(1:length(tileswpts),
-                 FUN = function(id) {
-                   roi <- tilearoundobj(tileswpts[[id]]$tile, 0.01) #0.01 degrees
-                   woody <- woody_vals_buffer(roi, tileswpts[[id]]$pts, 2000:2018, 500) #500 metres around each point
-                   return(woody)
-                 })
+saveRDS(woodyl, "./tmpdata/woodyl.rds")
 woody <- do.call(rbind, woodyl)
-# saveRDS(woody, "./tmpdata/woody.rds")
+saveRDS(woody, "./tmpdata/woody.rds")
 
 #### convert data into nicer format ready to import for modeling ####
 woody$SiteCode <- locs_wgs84$SiteCode
-# saveRDS(woody, "./private/data/remote_sensed/woody500.rds")
+saveRDS(woody, "./private/data/remote_sensed/woody500.rds")
