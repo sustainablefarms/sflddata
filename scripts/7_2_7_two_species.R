8ea0a878a632d445175e0b82fb474ac9d4739db9
Kassel Hingee <kassel.hingee@gmail.com>
2020-06-20 20:21:33 +1000

    make two species script work better again!

--- scripts/7_2_7_two_species.R
+++ scripts/7_2_7_two_species.R
@@ -5,123 +5,54 @@ sort(colMeans(indata$insampledata$yXobs[, indata$species]))
 
 species <- c("Galah", "Willie Wagtail") #all seen in about 0.3 of visits
 
-interceptsonly <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1",
-  ObsFmla = "~ 1",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_intercepts.rds"
-)
+modelspecs <- list(
+  interceptsonly = list(OccFmla = "~ 1",
+                        ObsFmla = "~ 1"),
+  ms =  list(OccFmla = "~ 1 + ms",
+             ObsFmla = "~ 1"),
+  os_ms =  list(OccFmla = "~ 1 + os + ms",
+                ObsFmla = "~ 1"),
+  os_ms_nm       = list(OccFmla = "~ 1 + os + ms + NMdetected",
+                        ObsFmla = "~ 1 "),
+  os_ms_nm_gc    = list(OccFmla = "~ 1 + os + ms + NMdetected + gc",
+                        ObsFmla = "~ 1 "),
+  os_msnm_gc     = list(OccFmla = "~ 1 + os + ms * NMdetected + gc",
+                        ObsFmla = "~ 1 "),
+  msnm_time      = list(OccFmla = "~ 1 + ms * NMdetected ",
+                        ObsFmla = "~ 1 + MeanTime"),
+  msnm_time_temp = list(OccFmla = "~ 1 + ms * NMdetected",
+                        ObsFmla = "~ 1 + MeanTime + MeanTemp"),
+  msnm_timetemp  = list(OccFmla = "~ 1 + ms * NMdetected",
+                        ObsFmla = "~ 1 + MeanTime * MeanTemp"))
 
+modelspecs <- sapply(names(modelspecs), function(x) {
+  modspec <- modelspecs[[x]]
+  modspec$filename <- paste0("./tmpdata/twospecies_", x,".rds")
+  return(modspec)},
+  USE.NAMES = TRUE,
+  simplify = FALSE)
 
-ms <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1 + ms",
-  ObsFmla = "~ 1",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_ms.rds"
-)
-
-os_ms <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1 + os + ms ",
-  ObsFmla = "~ 1",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_os_ms.rds"
-)
-
-os_ms_nm <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1 + os + ms + NMdetected",
-  ObsFmla = "~ 1",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_os_ms_nm.rds"
-)
-
-os_ms_nm_gc <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1 + os + ms + NMdetected + gc",
-  ObsFmla = "~ 1",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_os_ms_nm_gc.rds"
-)
-
-os_msnm_gc <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1 + os + ms * NMdetected + gc",
-  ObsFmla = "~ 1",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_os_msnm_gc.rds"
-)
-
-
-msnm_time <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1 + ms * NMdetected",
-  ObsFmla = "~ 1 + time",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_msnm_time.rds"
-)
-
-msnm_time_temp <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1 + ms * NMdetected",
-  ObsFmla = "~ 1 + time + temp",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_msnm_time_temp.rds"
-)
-
-msnm_timetemp <- run.detectionoccupancy(
-  Xocc = indata$insampledata$Xocc,
-  yXobs = indata$insampledata$y,
-  species = species,
-  ModelSite = "ModelSiteID",
-  OccFmla = "~ 1 + ms * NMdetected",
-  ObsFmla = "~ 1 + time * temp",
-  nlv = 0,
-  initsfunction = function(x, y){return(NULL)},
-  MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
-  filename = "./tmpdata/twospecies_msnm_timetemp.rds"
-)
-
+cl <- parallel::makeCluster(1)#length(modelspecs))
+parallel::clusterEvalQ(cl, devtools::load_all())
+parallel::clusterExport(cl, c("indata", "species"))
+fittedmods <- pbapply::pblapply( 
+                        modelspecs,
+                        function(x) {
+                          fit <- run.detectionoccupancy(
+                            Xocc = indata$insampledata$Xocc,
+                            yXobs = indata$insampledata$yXobs,
+                            species = species,
+                            ModelSite = "ModelSiteID",
+                            OccFmla = x$OccFmla,
+                            ObsFmla = x$ObsFmla,
+                            nlv = 0,
+                            initsfunction = function(chain, indata){return(NULL)},
+                            MCMCparams = list(n.chains = 2, adapt = 1000, burnin = 10000, sample = 500, thin = 40),
+                            filename = x$filename
+                          )
+                          return(fit)
+                        },
+                        cl = cl
+                        )
+parallel::stopCluster(cl)
 
