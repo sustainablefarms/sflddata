4aee23cb9bb89bdf6a16a346ebe4ec885c9d7bb9
Kassel Hingee <kassel.hingee@gmail.com>
2020-06-20 22:25:46 +1000

    moved working _B gpp script to be the usual gpp script: 1_13_bytile_gpp.R

--- scripts/1_13_bytile_gpp.R
+++ scripts/1_13_bytile_gpp.R
@@ -5,14 +5,14 @@ locs <- read.csv("private/data/clean/site_locations_cleaned.csv", stringsAsFacto
 locs_wgs84 <- st_as_sf(locs, coords = c("MeanLON", "MeanLAT"))
 locs_wgs84 <- st_set_crs(locs_wgs84, 4326) #4326 is the epsg code for WGS84, make this default crs I'll return to
 
-tileswpts <- divide_into_tiles(locs_wgs84, cellsize = 0.05, buffer = 0.01)  #0.01 degrees
-# saveRDS(tileswpts, file = "./tmpdata/gppl_tileswpts.rds")
-# tileswpts <- readRDS("./tmpdata/gppl_tileswpts.rds")
+tileswpts <- divide_into_tiles(locs_wgs84, cellsize = 1, buffer = 0.01)  #0.01 degrees
+# saveRDS(tileswpts, file = "./tmpdata/gppl_tileswpts_B.rds")
+tileswpts <- readRDS("./tmpdata/gppl_tileswpts_B.rds")
 
 #### read RS rasters ####
 # empty gppl object
 gppl <- lapply(tileswpts, function(x) return(NULL))
-# gppl <- readRDS("./tmpdata/gppl.rds")
+gppl <- readRDS("./tmpdata/gppl_B.rds")
 uncompleted <- vapply(gppl, is.null, FUN.VALUE = FALSE)
 attempts <- 0
 rasterOptions(tmpdir = "/media/kassel/Seagate1TB/tmpdir2/")
@@ -21,26 +21,24 @@ while(any(uncompleted) && attempts <= 50){
   gppl[uncompleted][1:5] <- pbapply::pblapply(tileswpts[uncompleted][1:5],
                     FUN = function(x) {
                       gpp <- NULL
-                      try(gpp <- gpp_vals(x$tile, x$pts, 2000:2019))
+                      tryCatch(gpp <- gpp_vals(x$tile, x$pts, 2000:2019),
+                           error = function(e) if (!any(grepl("NetCDF: I/O failure", e))){warning(e)})
                       gc()
                       if (!is.null(gpp)){gpp$SiteCode <- x$pts$SiteCode}
+                      # Sys.sleep(10)
                       return(gpp)
                     })
   uncompleted <- vapply(gppl, is.null, FUN.VALUE = FALSE)
   attempts <- attempts + 1
 }
 
-saveRDS(gppl, "./tmpdata/gppl.rds")
+saveRDS(gppl, "./tmpdata/gppl_B.rds")
 gpp <- do.call(rbind, gppl)
-saveRDS(gpp, "./tmpdata/gpp.rds")
+saveRDS(gpp, "./tmpdata/gpp_B.rds")
 
 #### convert data into nicer format ready to import for modeling ####
-gpp$mean <- rowMeans(gpp)
-gpp$SiteCode <- locs_wgs84$SiteCode
 saveRDS(gpp, "./private/data/remote_sensed/8d_gpp.rds")
 
-extractedrows <- vapply(gppl, nrow, FUN.VALUE = 3)
-numptsintile <- vapply(tileswpts, function(x) nrow(x$pts), FUN.VALUE = 3)
 
 # the following suggests a buffer of 0.01 degrees will be plenty for 500m buffers
 # st_distance(locs_sf[1, ], locs_sf[2, ])
