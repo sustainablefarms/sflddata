dfb03f520e50618802b8587f2edc4e9b06f3d38c
Kassel Hingee <kassel.hingee@gmail.com>
2020-07-24 14:49:29 +1000

    experimenting with exporting model draws - but these are *not* saved in repo yet

--- scripts/7_3_02_extract_occpars_bestmodel.R
+++ scripts/7_3_02_extract_occpars_bestmodel.R
@@ -6,3 +6,7 @@ rownames(u.b) <- fit$species
 colnames(u.b) <- colnames(fit$data$Xocc)
 saveRDS(list(XoccProcess = XoccProcess, 
              u.b = u.b), "./private/models/7_3_02_clim_someclimate_year_woody500m_msnm_det1stO_extract.rds")
+
+draws <- do.call(rbind, fit$mcmc)
+draws_sub <- draws[, grepl("^u.b", colnames(draws))]
+saveRDS(draws_sub, file = "./private/models/7_3_02_clim_someclimate_year_woody500m_msnm_det1stO_draws.rds")
