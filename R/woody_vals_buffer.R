1dda5ff4359d560fc2a89c1f4db1ea55cea6b708
Kassel Hingee <kassel.hingee@gmail.com>
2020-10-12 13:01:30 +1100

    telling R which package to use

--- R/woody_vals_buffer.R
+++ R/woody_vals_buffer.R
@@ -7,22 +7,22 @@
 # woody_vals_buffer(tilearoundpt(locs_GDA94AA[1, ], buffer = 1000), locs_GDA94AA[1, ], 2018, buffer = 500)
 #' @export
 woody_vals_buffer <- function(roi, pts, years, buffer){
-  suppressMessages(iscovered <- (vapply(st_covered_by(pts, roi), length, FUN.VALUE = 1.1) >= 1))
+  suppressMessages(iscovered <- (vapply(sf::st_covered_by(pts, roi), length, FUN.VALUE = 1.1) >= 1))
   stopifnot(all(iscovered))
-  roi_sp <- as_Spatial(st_transform(roi, 3577)) #woody_b is in epsg:3577, which is GDA94
-  pts_sp <- as_Spatial(st_transform(pts, 3577))
+  roi_sp <- sf::as_Spatial(sf::st_transform(roi, 3577))
+  pts_sp <- sf::as_Spatial(sf::st_transform(pts, 3577))
   
   woody_b <- brick_woodycover(roi_sp, years) #in epsg:3577, which is GDA94
   wf <- raster::focalWeight(woody_b, d = buffer, type = "circle") # produces a warnings, but the matrix output still looks good
   woody_vals_l <- lapply(1:nrow(pts_sp), 
        function(id){
-         ptbuffer <- buffer(pts_sp[id, ], width = buffer * 1.2)
-         smallbrick <- crop(woody_b, ptbuffer)
+         ptbuffer <- raster::buffer(pts_sp[id, ], width = buffer * 1.2)
+         smallbrick <- raster::crop(woody_b, ptbuffer)
          smallbrick[smallbrick == 157] <- 0
          smallbrick[smallbrick > 100] <- NA # I don't know what the other values above 100 mean, and if there are any
          woody_bs <- focal_bylayer(smallbrick, wf, fun = sum)
          names(woody_bs) <- names(woody_b)
-         woody_vals <- extract(woody_bs, pts_sp[id, ], df = TRUE)
+         woody_vals <- raster::extract(woody_bs, pts_sp[id, ], df = TRUE)
        })
   woody_vals <- do.call(rbind, woody_vals_l)
   # woody_vals <- extract(woody_bs, pts_sp, df = TRUE)
