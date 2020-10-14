81afb1f8ffb948b187e8c356f0cf3db9f5b81246
Kassel Hingee <kassel.hingee@gmail.com>
2020-10-15 07:38:19 +1100

    added some help info for latlon2webdata - but still not being added to namespace

--- R/latlon2webdata.R
+++ R/latlon2webdata.R
@@ -1,4 +1,5 @@
-#' @title Functions to acquire data for model predictors that is available online.
+#' @title Download online data for spatial locations that are needed for particular models.
+#' @description Downloads required data for spatial locations in order to run model.
 #' @param coords An [sf] object with coordinates.
 #' @param years Years of woody cover to use.
 #' @param model A character code providing the model for which data to acquire
@@ -8,7 +9,8 @@
 #' locs <- sf::st_as_sf(locations, coords = c("MeanLON", "MeanLAT"), crs = 4326)
 #' coords <- locs[ locs$StudyCode == "Nanangroe Natural Experiment", ]
 #' fromwebdata <- ll2webdata(coords, 2018)
-
+#' @return A dataframe with columns of woody500m each year, and climatic information.
+#' @export
 ll2webdata <- function(coords, years, model = "7_4"){
   stopifnot(model == "7_4")
   stopifnot(sf::st_crs(coords) == sf::st_crs(4326))
