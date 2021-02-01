f79694a51cf71c3b556ac60de8c86bcdd3b54a8e
Kassel Hingee <kassel.hingee@gmail.com>
2021-02-01 17:35:10 +1100

    app extraction of climatedata uses ll2worldclim() function

--- scripts/6_2_app_worldclim_data.R
+++ scripts/6_2_app_worldclim_data.R
@@ -5,50 +5,13 @@ sa2_points <- sa2_points[, c("longitude", "latitude", "label", "state", "color")
 # NOTE: This script will drop a file called wc0.5 in your home directory
   # you can delete it when done
 
-# get location for worldclim data
-library(raster)
+locs <- sf::st_as_sf(sa2_points, coords = c("longitude", "latitude"), crs = 4326)
+result_df <- sflddata::ll2worldclim(locs)
 
-# get a set of cordinates that fall within four tiles that our data could be in
-lookup_coordinates <- data.frame(
-  latitude = c(-28, -28, -34, -34),
-  longitude = c(144, 152, 144, 152))
-
-# get data and extract relevant points
-worldclim_list <- lapply(
-  split(lookup_coordinates, seq_len(4)),
-  function(a){
-    temp_data <- raster::getData(
-      name = "worldclim",
-      download = TRUE,
-      res = 0.5,
-      var = "bio",
-      lon = a$longitude,
-      lat =  a$latitude)
-    return(extract(temp_data, sa2_points[, c("longitude", "latitude")]))
-  })
-
-# look up which list entry to get each datum from
-row_index <- apply(
-  do.call(cbind,
-    lapply(worldclim_list, function(a){apply(a, 1, function(x){all(!is.na(x))})})),
-  1,
-  which)
-
-# extract correct data for each row
-result_df <- as.data.frame(do.call(rbind,
-  lapply(seq_along(row_index), function(a){worldclim_list[[row_index[a]]][a, ]})
-))
-# any(is.na(result_df)) # == FALSE
-
-# put nice names of data
-climnames <- readRDS("../private/data/clean/7_2_10_climate_names_table.rds")
-namemap <- tibble::deframe(climnames[, c("code", "shortname")])
-colnames(result_df) <- namemap[gsub("_.*", "", colnames(result_df))]
 
 # cut down to only data that the model needs
 result_df <- result_df[, c("AnnPrec", "MaxTWarmMonth", "MinTColdMonth", "PrecSeasonality")]
 
 result_df <- cbind(sa2_points, result_df)
-
 
 saveRDS(result_df, "../../farm_biodiversity_app/data/sa2_points_climate.rds")
