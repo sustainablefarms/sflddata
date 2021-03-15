26b638b2df70e4b62473e7fe1c3855080014db34
Kassel Hingee <kassel.hingee@gmail.com>
2021-03-15 18:02:33 +1100

    ordered factor saved and exported ordfactby() function

---
+++ R/ordfactby.R
@@ -0,0 +1,23 @@
+#' @title Turn a column into an ordered factor
+#' @description Converts a column into an ordered factor.
+#' @param data a data frame or tibble
+#' @param column the column to convert to an ordered factor
+#' @param by the column used to order `column`
+#' @param ... expressions passed to `dplyr::filter`. Only rows satisfying these expressions are used to order `column`,
+#' @return The dataframe with `column` converted to an ordered factor that is ordered according to `by`
+#' @details `column`, `by`, and `...` use tidyselect (e.g. column names do not need to be quoted)
+#' @examples 
+#' library(tidyr)
+#' mtcars$Model <- rownames(mtcars)
+#' ordfactby(mtcars, Model, gear)  #turns column Model into an ordered factor ordered by gear
+#' @export
+ordfactby <- function(data, column, by, ...){
+  specorder <- data %>%
+    dplyr::filter(...) %>%
+    dplyr::select({{ column }}, {{ by }}) %>% 
+    dplyr::arrange({{ by }}) %>% 
+    dplyr::select({{ column }}) %>%
+    unlist()
+  data <- dplyr::mutate(data, {{column}} := factor({{ column }}, levels = specorder, ordered = TRUE))
+  return(data)
+}
