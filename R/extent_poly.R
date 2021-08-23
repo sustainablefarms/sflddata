e261fe3485dd03f5ee3bd8d14a3807ea4d6cb1bc
Kassel Hingee <kassel.hingee@gmail.com>
2021-08-23 19:23:34 +1000

    added tile code to polygon to make plotting easy

--- R/extent_poly.R
+++ R/extent_poly.R
@@ -7,12 +7,19 @@
 extent_poly <- function(x){
   ras <- x
   bb <- raster::bbox(ras)
-  bb_pts <- rbind(t(bb)[1, ],
-   diag(bb),
-   t(bb)[2, ],
-   bb[c(3, 2)],
-   t(bb)[1, ])
-  poly <- sf::st_polygon(x = list(bb_pts), dim = "XY")
+  poly <- bb_to_poly(bb)
   roi <- sf::st_as_sf(sf::st_sfc(poly), crs = raster::crs(ras))
   return(roi)
 }
+
+bb_to_poly <- function(bb){
+  bb_pts <- rbind(t(bb)[1, ],
+                  diag(bb),
+                  t(bb)[2, ],
+                  bb[c(3, 2)],
+                  t(bb)[1, ])
+  poly <- sf::st_polygon(x = list(bb_pts), dim = "XY")
+  return(poly)
+}
+
+
--- R/get_tilecodes.R
+++ R/get_tilecodes.R
@@ -20,3 +20,44 @@ get_tilecodes <- function(spobj){
   names(tilecodes) <- tilecodes
   return(tilecodes)
 }
+
+#' @examples
+#' tilecodes <- unlist(read.csv(system.file("wcftilecodes.txt", package = "sflddata")))
+#' names(tilecodes) <- NULL
+#' tiles <- tiles_as_sf(tilecodes)
+#' ausstates <- readRDS("./private/data/basemaps/ausstates.rds") 
+#' sf::st_crs(ausstates) <- sf::st_crs(ausstates)
+#' ausstates <- sf::st_transform(ausstates, sf::st_crs(tiles))
+#' # check that tiles cover all land mass
+#' library(ggplot2)
+#' ggplot() +
+#'   geom_sf(aes(), fill = "red", data = ausstates) +
+#'   geom_sf(aes(), data = tiles)
+
+#' @describeIn get_tilecodes Make sf polygons showing the tiles
+#' @param tilecodes A list of tile codes.
+#' @export
+tiles_as_sf <- function(tilecodes){
+  tilestep <- 100000
+  splitted <- strsplit(tilecodes, split = "_")
+  xmins <- as.numeric(vapply(splitted, function(x) x[[1]], FUN.VALUE = "6")) * tilestep
+  ymins <- as.numeric(vapply(splitted, function(x) x[[2]], FUN.VALUE = "6")) * tilestep
+  xmaxs <- xmins + tilestep
+  ymaxs <- ymins + tilestep
+  tile_extents <- data.frame(
+    tile = tilecodes, xmin = xmins, ymin = ymins, xmax = xmaxs, ymax = ymaxs)
+  polys <- apply(tile_extents[, -1], MARGIN = 1, function(x) {
+    bb <- minmaxs_to_bb(x)
+    poly <- bb_to_poly(bb)
+    return(poly)})
+  polys_sfc <- sf::st_sfc(polys, crs = 3577)
+  tiles <- sf::st_sf(tile_extents, polys_sfc)
+  return(tiles)
+}
+
+minmaxs_to_bb <- function(x){
+  bbform <- matrix(x, nrow = 2, ncol = 2, byrow = FALSE)
+  colnames(bbform) <- c("min", "max")
+  rownames(bbform) <- c("s1", "s2")
+  return(bbform)
+}
