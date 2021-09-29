10c584fe7408244a08c72ce2c699cbb394459fda
Kassel Hingee <kassel.hingee@anu.edu.au>
2021-09-29 19:59:20 +1000

    fetch woody cover brick with new root directory is complete

--- R/fetch_woody_cover_brick.R
+++ R/fetch_woody_cover_brick.R
@@ -2,6 +2,8 @@
 #' @description Uses the files stored at the THREDDS server at NCI.
 #' @param spobj Spatial* or sf object that informs extents of the raster to extract
 #' @param years Years of data to extract
+#' @param rootdir Where the tiles are located. Typically [fillmismatch]http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC,
+#'  however I have had need to download the tiles wholescale using [download_WCF_tiles()] and access them locally.
 #' @return A raster brick with extent equal or larger than \code{extent(spobj)}, snapped to the cells of the raster data.
 #' The projection of the returned raster is EPSG:3577, which is GDA94.
 #' @details Combines the tile file naming convention with [raster_wcflike()] and [fetch_brick_Albers()]
@@ -12,9 +14,9 @@
 #'                            1590555, -3664500,
 #'                            1590555, -3670100), byrow = TRUE, ncol = 2)
 #' demoshape <- sf::st_sfc(sf::st_polygon(x = list(polypts), dim = "XY"), crs = 3577)
-#' b <- fetch_woody_cover_brick(demoshape, 2011:2012)
+#' b <- fetch_woody_cover_brick(demoshape, 1990)
 #' @export
-fetch_woody_cover_brick <- function(spobj, years, rootdir = "http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC"){
+fetch_woody_cover_brick <- function(spobj, years, rootdir = "[fillmismatch]http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC"){
   
   b <- fetch_brick_Albers(spobj, years,
                      get_tile_filenames = function(tilecode, years){
@@ -24,7 +26,7 @@ fetch_woody_cover_brick <- function(spobj, years, rootdir = "http://dapds00.nci.
   return(b)
 }
 
-get_tile_filenames_WCF <- function(tilecode, years, rootdir = "http://dapds00.nci.org.au/thredds/dodsC/ub8/au/LandCover/DEA_ALC"){
+get_tile_filenames_WCF <- function(tilecode, years, rootdir = NULL){
   filelist <- build_filename_list(rootdir,
                                   tilecode,
                                   paste0("fc_metrics_", tilecode, "_"),
@@ -36,7 +38,7 @@ get_tile_filenames_WCF <- function(tilecode, years, rootdir = "http://dapds00.nc
 }
 
 tilereader_WCF <- function(filename){
-  ras <- withCallingHandlers(raster_wcflike(paste0("[fillmismatch]",filename), varname = "WCF"),
+  ras <- withCallingHandlers(raster_wcflike(filename, varname = "WCF"),
                              warning = function(w){
                                if (grepl("cannot process these parts of the CRS", w$message))
                                  tryInvokeRestart("muffleWarning") 
