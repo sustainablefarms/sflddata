89b12ca9baa9054a1002ce47e99bdff81086882a
Kassel Hingee <kassel.hingee@gmail.com>
2021-10-05 09:49:04 +1100

    commented out some lines in bytile woody veg script on July 16, I'm note sure why

--- scripts/1_14_bytile_woodyveg.R
+++ scripts/1_14_bytile_woodyveg.R
@@ -8,8 +8,8 @@ locs_wgs84 <- st_set_crs(locs_wgs84, 4326) #4326 is the epsg code for WGS84, mak
 # locs_wgs84 %>%
 #   filter(StudyCode == "Restoration Study") %>%
 #   st_bbox()
-# tileswpts <- divide_into_tiles(locs_wgs84, cellsize = 0.05, buffer = 0.01)
-tileswpts <- readRDS("./tmpdata/woodyl_tileswpts.rds")
+tileswpts <- divide_into_tiles(locs_wgs84, cellsize = 0.05, buffer = 0.01)
+# tileswpts <- readRDS("./tmpdata/woodyl_tileswpts.rds")
 tiles <- lapply(tileswpts, function(x) st_as_sf(x$tile))
 tilegrd <- do.call(rbind, tiles)
 # plot(tilegrd)
@@ -21,13 +21,15 @@ woodyl <- lapply(tileswpts, function(x) return(NULL))
 uncompleted <- vapply(woodyl, is.null, FUN.VALUE = FALSE)
 attempts <- 0
 
-rasterOptions(tmpdir = "/media/kassel/Seagate1TB/tmpdir/")
+# rasterOptions(tmpdir = "/media/kassel/Seagate1TB/tmpdir/")
 while(any(uncompleted) && attempts <= 5){
   cat("Attempting:", sum(uncompleted), "tiles.\n")
   woodyl[uncompleted] <- pbapply::pblapply(tileswpts[uncompleted],
                                          FUN = function(x) {
                                            woody <- NULL
                                            try(woody <- woody_vals_buffer(x$tile, x$pts, 2000:2018, 500))
+                                           # raster::removeTmpFiles(h = -1)
+                                           raster::showTmpFiles()
                                            gc()
                                            return(woody)
                                          })
